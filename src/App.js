48af3fe9103f2e01fd174a674c1cc406858e22ca
Hasan Khan <khan.hasan@hotmail.ca>
2023-03-16 01:57:59 -0400

    Can add multiple nodes now and added sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,106 @@
-import './App.css';
+import React, { useState, useRef, useCallback } from 'react';
+import ReactFlow, {
+  ReactFlowProvider,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  Controls,
+  Position
+} from 'reactflow';
+import 'reactflow/dist/style.css';
 
-function App() {
+import Sidebar from './Sidebar';
+
+import './index.css';
+
+import { initialEdges, initialNodes } from './nodes-and-edges';
+
+let id = 0;
+const getId = () => `dndnode_${id++}`;
+
+const DnDFlow = () => {
+  const reactFlowWrapper = useRef(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
+
+  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
+
+  const onDrop = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+      const type = event.dataTransfer.getData('application/reactflow');
+
+      // check if the dropped element is valid
+      if (typeof type === 'undefined' || !type) {
+        return;
+      }
+
+      const position = reactFlowInstance.project({
+        x: event.clientX - reactFlowBounds.left,
+        y: event.clientY - reactFlowBounds.top,
+      });
+
+      const nodeDefaults = {
+        sourcePosition: Position.Right,
+        targetPosition: Position.Left,
+        style: {
+          borderRadius: '100%',
+          backgroundColor: '#fff',
+          width: 50,
+          height: 50,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      };
+
+      const newNode = {
+        id: getId(),
+        type,
+        position,
+        data: { label: `Node` },
+        ...nodeDefaults,
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+      for (let i = 0; i < nodes.length; i++) {
+        console.log(nodes[i]);
+    } 
+    },
+    [reactFlowInstance]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    
+    <div className="dndflow">
+      <ReactFlowProvider>
+        <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onInit={setReactFlowInstance}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+            fitView
+          >
+            <Controls />
+          </ReactFlow>
+        </div>
+        <Sidebar />
+      </ReactFlowProvider>
     </div>
   );
-}
+};
 
-export default App;
+export default DnDFlow;
---
+++ src/Sidebar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+export default () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  };
+
+  return (
+    
+
+    <aside>
+      <head>
+      <meta charset="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossorigin="anonymous"
+        />
+      </head>
+      <h3>Dijkstra's Algorithm</h3>
+      <br></br>
+      <button type="button" class="btn btn-outline-success">
+              Run
+      </button>
+      <br></br>
+            <br></br>
+      <div class="input-group mb-3" style={{ width: 200 }}>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Edge Weight"
+                aria-label="Edge Weight"
+                aria-describedby="basic-addon2"
+              />
+              <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">
+                  Update
+                </button>
+                
+                </div>
+            </div>
+            <br></br>
+            <br></br>
+      <div className="description">You can drag these nodes to the pane on the right.</div>
+      <div className="dndnode input" onDragStart={(event) => onDragStart(event, 'default')} draggable>
+        Start Node
+      </div>
+      <div className="dndnode" onDragStart={(event) => onDragStart(event, 'default')} draggable>
+        Node
+      </div>
+      <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'default')} draggable>
+        End Node
+      </div>
+    </aside>
+    
+  );
+};
